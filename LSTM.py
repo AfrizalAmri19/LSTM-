2b05bfb82f7ced6216bf894036ceeea8fc5dc19d
AfrizalAmri19 <61112560+AfrizalAmri19@users.noreply.github.com>
2021-06-26 13:13:53 +0700

    Add files via upload

--- LSTM.py
+++ LSTM.py
@@ -73,43 +73,43 @@ trainPredict = model.predict(trainX)
 print(trainPredict)
 testPredict = model.predict(testX)
 print(testPredict)
-#
-# # # create empty table with 12 fields
+
+# create empty table with 12 fields
 # trainPredict_dataset_like = np.zeros(shape=(len(trainPredict), 23) )
-# # # put the predicted values in the right field
+# put the predicted values in the right field
 # trainPredict_dataset_like[:,0] = trainPredict[:,0]
-# # # inverse transform and then select the right field
-# trainPredict = scaler.inverse_transform(trainPredict_dataset_like)[:,0]
+# inverse transform and then select the right field
+#trainPredict = scaler.inverse_transform(trainPredict_dataset_like)[:,0]
 # trainPredict = np.array(trainPredict).reshape(-1,1,)
 # print(trainPredict)
 
-# invert predictions
-#trainPredict = scaler.inverse_transform(trainPredict)
+# invert predictions (big problem start here
+trainPredict = scaler.inverse_transform(trainPredict)
 trainY = scaler.inverse_transform([trainY])
 testPredict = scaler.inverse_transform(testPredict)
 testY = scaler.inverse_transform([testY])
 
 # # calculate root mean squared error
-# trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
-# print('Train Score: %.2f RMSE' % (trainScore))
-# testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
-# print('Test Score: %.2f RMSE' % (testScore))
+trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
+print('Train Score: %.2f RMSE' % (trainScore))
+testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+print('Test Score: %.2f RMSE' % (testScore))
+
+# shift train predictions for plotting
+trainPredictPlot = np.empty_like(dataset)
+trainPredictPlot[:, :] = np.nan
+trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+
+# shift test predictions for plotting
+testPredictPlot = np.empty_like(dataset)
+testPredictPlot[:, :] = np.nan
+testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
 #
-# # shift train predictions for plotting
-# trainPredictPlot = np.empty_like(dataset)
-# trainPredictPlot[:, :] = np.nan
-# trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
-#
-# # shift test predictions for plotting
-# testPredictPlot = np.empty_like(dataset)
-# testPredictPlot[:, :] = np.nan
-# testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
-#
-# # plot baseline and predictions
-# plt.plot(scaler.inverse_transform(dataset))
-# plt.plot(trainPredictPlot)
-# plt.plot(testPredictPlot)
-# plt.show()
+# plot baseline and predictions
+plt.plot(scaler.inverse_transform(dataset))
+plt.plot(trainPredictPlot)
+plt.plot(testPredictPlot)
+plt.show()
 
 # print(trainX, testX)
 # print(testX, testY)
